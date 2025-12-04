dbcfd00a81250c94a5f0f1aeebcce9a7ffdc05ef
LootingBery <ryborg.benjamin@gmail.com>
2025-12-04 13:32:11 +0100

    Implement task addition and deletion functionality with updated index template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,14 +9,38 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.timezone.utc.now)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
         return f'<Task {self.id}>'
 
-@app.route("/")
+@app.route("/", methods=['POST' , 'GET'])
 def index():
-    return render_template("index.html") 
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
