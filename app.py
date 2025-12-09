8dc56a0e9e304a84badcbebc594d2c04a975b3ef
LootingBery <ryborg.benjamin@gmail.com>
2025-12-09 10:42:39 +0100

    Add deadline feature to tasks and improve UI; update styles and templates

--- app.py
+++ app.py
@@ -1,60 +1,83 @@
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
+    deadline = db.Column(db.DateTime, nullable=True)
+
+    def is_overdue(self):
+        return self.deadline and datetime.now() > self.deadline
 
     def __repr__(self):
-        return f'<Todo {self.id}>'
+        return f"<Todo {self.id}>"
 
-@app.route("/", methods=['POST' , 'GET'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        content = request.form["content"]
+        deadline_raw = request.form.get("deadline")
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "Error adding your task"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-    
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+        deadline = (
+            datetime.fromisoformat(deadline_raw)
+            if deadline_raw else None
+        )
 
-    try:
-        db.session.delete(task_to_delete)
+        new_task = Todo(
+            content=content,
+            deadline=deadline
+        )
+
+        db.session.add(new_task)
         db.session.commit()
-        return redirect('/')
-    except:
-        return 'Error deleting your task'
-    
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+        return redirect("/")
+
+    tasks = Todo.query.order_by(
+        Todo.deadline.is_(None),
+        Todo.deadline,
+        Todo.date_created
+    ).all()
+
+    return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = Todo.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
 def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == "POST":
-        task.content = request.form['content']
+        task.content = request.form["content"]
+        deadline_raw = request.form.get("deadline")
+        task.deadline = (
+            datetime.fromisoformat(deadline_raw)
+            if deadline_raw else None
+        )
 
-        try:
-            db.session.commit()
-            return redirect("/")
-        except:
-            return "Error updating your task"
-    else:   
-        return render_template('update.html', task=task)
-    
+        db.session.commit()
+        return redirect("/")
+
+    return render_template("update.html", task=task)
+
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
